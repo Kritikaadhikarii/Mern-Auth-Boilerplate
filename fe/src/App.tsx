dd1a1103355db8060a627ebb1a2f4f977bb01636
Kritikaadhikarii <itskritikaaa@gmail.com>
2025-07-20 21:53:37 +0545

    add : create basic layout for auth

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import img from "./assets/image.png";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="bg-[#1E1E1E] min-h-screen flex flex-col md:flex-row gap-4 p-4">
+      <section
+        className=" rounded-sm w-full md:w-1/2 h-80 md:h-auto"
+        style={{
+          backgroundImage: `url(${img})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <section className="w-full md:w-[60%] h-48 md:h-full" />
+    </main>
+  );
 }
-
-export default App
